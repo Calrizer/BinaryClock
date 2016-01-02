9a0dc73a2d8bc05be83e05564d74c0455c520da4
Callum Drain <callumdrain@me.com>
2016-01-02 10:36:02 +0000

    Added base interface. Need to add logic.

--- CLOCK.pas
+++ CLOCK.pas
@@ -1,2 +1,38 @@
 program binaryClock;
 
+uses crt,dos;
+
+var hour,min,sec,millisec:word;
+
+current:integer;
+
+procedure showTime;
+begin
+
+gettime(hour,min,sec,millisec);
+
+repeat
+
+if (hour / current) mod 2 = {Finish this later}
+
+until current = 0;
+
+end;
+
+begin
+
+writeln('================[ Binary Clock ]==================');
+writeln;
+writeln('           32    16    8    4    2    1');
+writeln;
+writeln('Hour');
+writeln;
+writeln('Minute');
+writeln;
+writeln('Second');
+writeln;
+repeat
+showTime;
+delay(1000);
+until KeyPressed;
+end.
